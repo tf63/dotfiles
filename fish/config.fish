2380b4b1b218e86e555444ecdfe905fa3bfee389
tf63 <tfukuda9031@gmail.com>
2024-01-17 12:22:39 +0900

    fish config

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+set fish_color_cwd            brred
+set fish_color_host           brgreen
+set fish_color_host_remote    bryellow
+set fish_color_user           brblue
