15c2d01add2239e144d8218077f00d6a927be948
tf63 <74246282+tf63@users.noreply.github.com>
2024-06-02 01:56:34 +0900

    update kitty config

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # dotfiles
 
+kitty conf https://sw.kovidgoyal.net/kitty/conf/
+
 background https://sakuranouta.biz/fairy-tale-townscape/
--- cmd/link.sh
+++ cmd/link.sh
@@ -4,10 +4,10 @@
 # ln -sf ~/project/dotfiles/fish ~/.config/fish
 # ln -sf ~/project/dotfiles/git ~/.config/git
 
-ln -sf ~/project/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -sf ~/project/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -sf ~/project/dotfiles/vscode/tasks.json ~/Library/Application\ Support/Code/User/tasks.json
-ln -sf ~/project/dotfiles/vscode/snippets ~/Library/Application\ Support/Code/User/snippets
+# ln -sf ~/project/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+# ln -sf ~/project/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+# ln -sf ~/project/dotfiles/vscode/tasks.json ~/Library/Application\ Support/Code/User/tasks.json
+# ln -sf ~/project/dotfiles/vscode/snippets ~/Library/Application\ Support/Code/User/snippets
 
 # ln -sf ~/project/dotfiles/home/.gitconfig ~/.gitconfig
 # ln -sf ~/project/dotfiles/home/.zshrc ~/.zshrc
