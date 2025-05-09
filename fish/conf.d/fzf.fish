b9a27d7858ad4654e48e40b31e5e250c84bdad40
tf63 <74246282+tf63@users.noreply.github.com>
2025-05-10 06:29:25 +0900

    fix: move completion for fzf

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -1,4 +1,2 @@
-fzf --fish | source
-
 set -gx FZF_CTRL_T_OPTS "--walker-skip .git,node_modules,target,.venv,venv --preview 'bat -n --color=always {}' --bind 'ctrl-/:change-preview-window(down|hidden|)'"
 set -gx FZF_DEFAULT_OPTS "--walker-skip .git,node_modules,target,.venv,venv"
