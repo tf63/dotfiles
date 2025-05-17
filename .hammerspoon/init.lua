938a574d205e6987e0adf54542c7b946ac86183f
tf63 <74246282+tf63@users.noreply.github.com>
2025-05-17 22:34:32 +0900

    feat: update hammerspoon config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,10 +1,8 @@
--- kitty
--- hs.hotkey.bind({"ctrl"}, "t", function()
+-- example
+-- hs.hotkey.bind({"alt"}, "1", function()
 --     local kitty = hs.application.get("kitty")
 --     if kitty == nil then
 --         hs.application.launchOrFocus("/Applications/kitty.app")
---     elseif kitty:isFrontmost() then
---         kitty:hide()
 --     else
 --         local space = hs.spaces.focusedSpace()
 --         local win = kitty:focusedWindow()
@@ -12,17 +10,60 @@
 --         win:focus()
 --     end
 -- end)
+-- 別のディスプレイでアクティブになっている場合は除外したい
+
+-- kitty
+hs.hotkey.bind({"alt"}, "1", function()
+    local app = hs.application.get("kitty")
+
+    if app == nil then
+        hs.application.launchOrFocus("/Applications/kitty.app")
+    else
+        local space = hs.spaces.focusedSpace()
+        local win = app:focusedWindow()
+        hs.spaces.moveWindowToSpace(win, space)
+        win:focus()
+    end
+end)
+
+-- vscode
+hs.hotkey.bind({"alt"}, "2", function()
+    local app = hs.application.get("Code")
+
+    if app == nil then
+        hs.application.launchOrFocus("/Applications/Visual Studio Code.app")
+    else
+        local space = hs.spaces.focusedSpace()
+        local win = app:focusedWindow()
+        hs.spaces.moveWindowToSpace(win, space)
+        win:focus()
+    end
+end)
+
+-- arc
+hs.hotkey.bind({"alt"}, "3", function()
+    local app = hs.application.get("Arc")
+
+    if app == nil then
+        hs.application.launchOrFocus("/Applications/Arc.app")
+    else
+        local space = hs.spaces.focusedSpace()
+        local win = app:focusedWindow()
+        hs.spaces.moveWindowToSpace(win, space)
+        win:focus()
+    end
+end)
 
 -- Warp
--- hs.hotkey.bind({"ctrl"}, "t", function()
---     local warp = hs.application.get("Warp")
---     if warp == nil then
+-- hs.hotkey.bind({"alt"}, "1", function()
+--     local app = hs.application.get("Warp")
+--     if app == nil then
 --         hs.application.launchOrFocus("/Applications/Warp.app")
---     elseif warp:isFrontmost() then
---         warp:hide()
+--     elseif app:isFrontmost() then
+--         app:hide()
 --     else
 --         local space = hs.spaces.focusedSpace()
---         local win = warp:focusedWindow()
+--         local win = app:focusedWindow()
 --         hs.spaces.moveWindowToSpace(win, space)
 --         win:focus()
 --     end
