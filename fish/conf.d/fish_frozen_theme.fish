f85e068092ccbe85b0a24fed22e621ee181cfbc3
tf63 <74246282+tf63@users.noreply.github.com>
2026-05-31 01:52:29 +0900

    feat: update fish config

---
+++ fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,48 @@
+# This file was created by fish when upgrading to version 4.3, to migrate
+# theme variables from universal to global scope.
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+# See also the release notes for fish 4.3.0 (run `help relnotes`).
+
+set --global fish_color_autosuggestion 7C868A
+set --global fish_color_cancel FF9191
+set --global fish_color_command A9CF83
+set --global fish_color_comment FFFFD1
+set --global fish_color_cwd FFC1C1
+set --global fish_color_cwd_root A8D6FF
+set --global fish_color_end FF9191
+set --global fish_color_error D6A8FF
+set --global fish_color_escape E9D1FF
+set --global fish_color_history_current --bold
+set --global fish_color_host CFF9AB
+set --global fish_color_host_remote FFFFD1
+set --global fish_color_keyword
+set --global fish_color_match D1FFE9 --underline
+set --global fish_color_normal ECEFF4
+set --global fish_color_operator E9D1FF
+set --global fish_color_option
+set --global fish_color_param CFF9AB
+set --global fish_color_quote DDDD98
+set --global fish_color_redirection CFF9AB
+set --global fish_color_search_match --background=brblack
+set --global fish_color_selection --background=brblack
+set --global fish_color_status FF9191
+set --global fish_color_user D1E9FF
+set --global fish_color_valid_path --background=normal --underline
+set --global fish_pager_color_background
+set --global fish_pager_color_completion normal
+set --global fish_pager_color_description B3A06D
+set --global fish_pager_color_prefix normal
+set --global fish_pager_color_progress 4C566A --italics
+set --global fish_pager_color_secondary_background
+set --global fish_pager_color_secondary_completion 8FBCBB
+set --global fish_pager_color_secondary_description 8FBCBB
+set --global fish_pager_color_secondary_prefix 8FBCBB
+set --global fish_pager_color_selected_background --background=brblack
+set --global fish_pager_color_selected_completion D1E9FF
+set --global fish_pager_color_selected_description D1E9FF
+set --global fish_pager_color_selected_prefix D1E9FF
