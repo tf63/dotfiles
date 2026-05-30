9fe3aa3032e73626362836c2ab5ca2246de62619
tf63 <74246282+tf63@users.noreply.github.com>
2026-05-30 21:35:44 +0900

    feat: remove volta

--- fish/conf.d/profile.fish
+++ fish/conf.d/profile.fish
@@ -2,12 +2,6 @@ eval $(/opt/homebrew/bin/brew shellenv)
 
 source "$HOME/.local/bin/env.fish"
 
-# volta
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
-set -gx PATH "/usr/local/go/bin" $PATH
-set -gx PATH "$HOME/go/bin" $PATH
-
 # pnpm
 set -gx PNPM_HOME "$HOME/.pnpm"
 if not string match -q -- $PNPM_HOME $PATH
