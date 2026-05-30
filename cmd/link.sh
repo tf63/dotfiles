e9dbaf079d650085a88a1642603b5fbe2e09ce32
tf63 <74246282+tf63@users.noreply.github.com>
2026-05-31 01:52:37 +0900

    feat: update link command

--- cmd/link.sh
+++ cmd/link.sh
@@ -1,18 +1,19 @@
 #!/opt/homebrew/bin/fish
-# ln -sf ~/project/dotfiles/.hammerspoon ~/.hammerspoon
-# ln -sf ~/project/dotfiles/karabiner ~/.config/karabiner
-# ln -sf ~/project/dotfiles/fish ~/.config/fish
-# ln -sf ~/project/dotfiles/git ~/.config/git
+ln -sf ~/projects/dotfiles/.hammerspoon ~/.hammerspoon
+ln -sf ~/projects/dotfiles/karabiner ~/.config/karabiner
+ln -sf ~/projects/dotfiles/fish ~/.config/fish
+ln -sf ~/projects/dotfiles/git ~/.config/git
+ln -sf ~/projects/dotfiles/kitty ~/.config/kitty
 
-# ln -sf ~/project/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-# ln -sf ~/project/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-# ln -sf ~/project/dotfiles/vscode/tasks.json ~/Library/Application\ Support/Code/User/tasks.json
-# ln -sf ~/project/dotfiles/vscode/snippets ~/Library/Application\ Support/Code/User/snippets
+ln -sf ~/projects/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sf ~/projects/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -sf ~/projects/dotfiles/vscode/tasks.json ~/Library/Application\ Support/Code/User/tasks.json
+ln -sf ~/projects/dotfiles/vscode/snippets ~/Library/Application\ Support/Code/User/snippets
 
-# ln -sf ~/project/dotfiles/home/.gitconfig ~/.gitconfig
-# ln -sf ~/project/dotfiles/home/.zshrc ~/.zshrc
-# ln -sf ~/project/dotfiles/home/.profile ~/.profile
+ln -sf ~/projects/dotfiles/home/.gitconfig ~/.gitconfig
+ln -sf ~/projects/dotfiles/home/.zshrc ~/.zshrc
+ln -sf ~/projects/dotfiles/home/.profile ~/.profile
 
-# ln -sf ~/project/dotfiles/mise/config.toml ~/.config/mise/config.toml
+ln -sf ~/projects/dotfiles/mise/config.toml ~/.config/mise/config.toml
 
-# ln -sf ~/project/dotfiles/nvim ~/.config/nvim
+ln -sf ~/projects/dotfiles/nvim ~/.config/nvim
