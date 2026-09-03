b797795fc15cb0508ade844bcf66a8d317810c55
tf63 <74246282+tf63@users.noreply.github.com>
2026-09-03 10:22:14 +0900

    feat: update fzf settings

--- fish/conf.d/fzf.fish
+++ fish/conf.d/fzf.fish
@@ -1,19 +1,35 @@
-set -gx FZF_CTRL_T_OPTS "--walker-skip .git,node_modules,target,.venv,venv --preview 'bat -n --color=always {}' --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+set -gx FZF_CTRL_T_OPTS "--walker-skip .git,node_modules,target,.venv,venv"
 set -gx FZF_DEFAULT_OPTS "--walker-skip .git,node_modules,target,.venv,venv"
 
 function __fzf_open_file
-    set -l selected (fzf)
+    set -l selected (fzf --height 40% --reverse --preview 'bat -n --color=always {}')
     if test -n "$selected"
         commandline -i -- $selected
     end
+
+    commandline -f repaint
 end
 
-function __fzf_ghq
-    set repo (ghq list -p | fzf --height 40% --reverse --preview 'ls {}')
-    if test -n "$repo"
-        cd $repo
+function __fzf_open_branch
+    set -l selected (git branch -a --format='%(refname:short)' | fzf --height 40% --reverse --preview 'git log -5 --oneline --decorate {}')
+
+    if test -n "$selected"
+        commandline -i -- $selected
     end
+
+    commandline -f repaint
+end
+
+function __fzf_open_repository
+    set -l repo (ghq list -p | fzf --height 40% --reverse --preview 'ls {}')
+
+    if test -n "$repo"
+        commandline -i -- "cd $repo"
+    end
+
+    commandline -f repaint
 end
 
 bind \cT '__fzf_open_file'
-bind \cg '__fzf_ghq'
+bind \cb '__fzf_open_branch'
+bind \cg '__fzf_open_repository'
